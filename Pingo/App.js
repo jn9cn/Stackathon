5f1d5e3b1c869d45e601484f921ffcf9823b25c8
Jannine Chan <jannine.chan@gmail.com>
2018-02-13 18:05:17 -0500

    Update App.js

--- Pingo/App.js
+++ Pingo/App.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 import { Location, Permissions, Notifications } from 'expo';
 import MapView from 'react-native-maps';
-// import BgGeo from './BkgndGeolocation';
 import BackgroundGeolocation from "react-native-background-geolocation";
-// import { mapStyle } from './MapStyle';
 import { Places } from './seed';
 import { localNotification, schedulingOptions } from './LocalNotification';
 
@@ -37,70 +35,33 @@ export default class App extends Component {
 
   };
 
-  // extras: push notification, google oauth sign-in, stacks from onboard to use / text disappears
-  // background geolocation, animated zooms
-  // list of vistied / to visit in a tab
-
-  // 1. get user's current location
-
-  // 2. get user's places of interest
-  // 2a. access Google Bookmarks xml / json export
-  // 2b. parseInt() xml for lat/lng data (only for locations within MapView)
-
-  // map marker for array (array for rn-maps)
-  // render markers for places only visible in viewport
-
-  // if location changes, build geofence around user marker; after 3 min clear geofence and build another one around user
-
-  // explore push notification and geofencing vs distance matrix
-
-  // 3. evaluate if user is within 10 min walking distance from places of interest
-  // 3a. using Google Maps Distance Matrix API, if no places are within a 10 min walk, end
-  // 3b. else, if more than one place is within a 10 min walk, batch list them in a push notifcation
-  // 3c. else, if only one place is within a 10 min walk, send push notification
-
-  // 4. end background process and repeat in 10-15 min
-
-  // get user's location
-
   componentWillMount() {
-    console.log("component WILL mount 64")
     this._getNotificationAsync();
     this._getLocationAsync();
   }
 
   componentDidMount() {
-    console.log("component DID mount 70")
     this.watchId = Location.watchPositionAsync({enableHighAccuracy: true, timeInterval: 1000}, (position) => {
-      console.log("REGION SET 72")
       this.setState({region: {
           latitude: position.coords.latitude,
           longitude: position.coords.longitude,
           latitudeDelta: 0.00922*1.5,
           longitudeDelta: 0.00421*1.5
       }})
-      console.log("LOCATION SET 79")
       this.setState({ location: {
         coords: {
           latitude: position.coords.latitude,
           longitude: position.coords.longitude,
         }
       }})
-      // console.log("LOAD MARKERS")
-      // this._loadMarkers();
-      
-// NOTE: UMCOMMENT THIS BACK AFTER TESTING
-      console.log("GET DISTANCE MATRIX called 88")
       this.getDistanceMatrix();
   })}
 
   componentWillUnmount() {
-    console.log("component WILL UNmount 93")
     Location.clearWatch(this.watchId);
   }
 
   onRegionChange(region) {
-    console.log("ON REGION CHANGE 98")    
     this.setState({ region })
   }
 
@@ -127,10 +88,9 @@ export default class App extends Component {
     }
   }
   
-  // get user's places of interest
+  // Get user's places of interest
 
-  _loadMarkers = () => {
-    console.log("LOAD MARKERS 128")    
+  _loadMarkers = () => {  
     let latitude, longitude, title;
     let markers = [];
     Places.features.map(
@@ -143,20 +103,16 @@ export default class App extends Component {
     this.setState({ markers })
   }
 
-  _handleButtonPress = () => {
-    console.log("BUTTON PRESS 142")    
-    // update to only load markers within region
+  _handleButtonPress = () => {  
+    // Update to only load markers within region
       this._loadMarkers();
       Alert.alert(
         'You did it!',
         'Your places are ready to be discovered',
       );
-
   };
 
   moveMapToLocation = () => {
-    console.log("move map to loc 152")
-    // this._map.fitToElements(true);
     this.setState({
       region: {
         latitudeDelta: 0.002,
@@ -165,25 +121,18 @@ export default class App extends Component {
         longitude: this.state.location.coords.longitude
       }
     })
-    // console.log("GET DISTANCE MATRIX called 168")
-    // this.getDistanceMatrix();
-    console.log("GET DISTANCE MATRIX called 88")
     this.getDistanceMatrix();
     this.firePushNotification();
   }
 
   getDistanceMatrix = () => {
-    // fire every 30 seconds, 30000ms
-    console.log("GET DISTANCE MATRIX 165")    
     let distance = require('react-native-google-matrix');
     let usersLoc = this.state.location.coords;
     let markers = this.state.markers;
     let closeBy = [];
 
-    // map over markers
+    // Map over markers
     markers.map(elem => {
-      // takes arguments and a callback
-      console.log("in makers.map 173", elem.title)
       distance.get(
         {
           origin: usersLoc.latitude+","+usersLoc.longitude,
@@ -191,14 +140,11 @@ export default class App extends Component {
         },
         function(err, data) {
           if (err) return console.log("hitting error 181", err);
-          // console.log("GOOGLEMATRIX***", data, "***", elem);
           if (data.distanceValue < 600) {
-            console.log("~~~10 MIN WALK AWAY~~~", elem)
             return closeBy.push(elem);
         }
       })
     })
-    console.log("closeBy state", this.state.closeBy, "closeBy", closeBy)
       this.setState({ closeBy })
       if (this.state.closeBy.length) {
         this.firePushNotification();
@@ -224,21 +170,7 @@ export default class App extends Component {
     Notifications.presentLocalNotificationAsync(localNotification);
   }
 
-  // iterate through markers and return array of markers that are within 10 min walking distance
-  // push all markers to alert / push msg
-  // run every change in location (later on by movement / time interval)
-
-  // geofence: for all locations with 1 mi, put circular geofences around user and markers, radii = 0.3 m
-  // if user circle collides with marker circle (distance between centers < 0.6 m), 
-  // push all markers with collisions to alert / push notifcation message and send the message
-
-  // when user location is 10 min walk away from a Google Place (geofence), ping user
-  // note: if more than one Google Place is a 10 min walk away, push them into same notifcation and order them closest to farthest
-
   render() {
-    // console.log("MARKER*****154", this.state.markers[0])
-    // console.log("REGION^*^*^155", this.state.region)
-    // console.log("LOCATION^^^^156", this.state.location)
     
     let text = 'Waiting...';
     if (this.state.errorMessage) {
@@ -306,34 +238,3 @@ const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject,
   },
 });
-
-// region={this.state.mapRegion}
-// Google Maps Distance Matrix API Key: AIzaSyC4Qtz8UJspogS9ccNm4xvi3HOQOmgPgYs
-
-// Google Maps Geocoding API Key: AIzaSyBPQZxYVs18P_5NeAHLXvVfQQ1D1LCB0gM
-
-// Google Maps Static Maps API Key: AIzaSyAAIyNzilS70CMpZKbCvuI1cHHZFrO-X0E
-//https://maps.googleapis.com/maps/api/staticmap?key=YOUR_API_KEY&center=40.707409939566155,-74.00906357724608&zoom=16&format=png&maptype=roadmap&style=element:geometry%7Ccolor:0xebe3cd&style=element:labels.text.fill%7Ccolor:0x523735&style=element:labels.text.stroke%7Ccolor:0xf5f1e6&style=feature:administrative%7Celement:geometry.stroke%7Ccolor:0xc9b2a6&style=feature:administrative.land_parcel%7Celement:geometry.stroke%7Ccolor:0xdcd2be&style=feature:administrative.land_parcel%7Celement:labels.text.fill%7Ccolor:0xae9e90&style=feature:landscape.natural%7Celement:geometry%7Ccolor:0xdfd2ae&style=feature:poi%7Celement:geometry%7Ccolor:0xdfd2ae&style=feature:poi%7Celement:labels.text.fill%7Ccolor:0x93817c&style=feature:poi.park%7Celement:geometry.fill%7Ccolor:0xa5b076&style=feature:poi.park%7Celement:labels.text.fill%7Ccolor:0x447530&style=feature:road%7Celement:geometry%7Ccolor:0xf5f1e6&style=feature:road.arterial%7Celement:geometry%7Ccolor:0xfdfcf8&style=feature:road.highway%7Celement:geometry%7Ccolor:0xf8c967&style=feature:road.highway%7Celement:geometry.stroke%7Ccolor:0xe9bc62&style=feature:road.highway.controlled_access%7Celement:geometry%7Ccolor:0xe98d58&style=feature:road.highway.controlled_access%7Celement:geometry.stroke%7Ccolor:0xdb8555&style=feature:road.local%7Celement:labels.text.fill%7Ccolor:0x806b63&style=feature:transit.line%7Celement:geometry%7Ccolor:0xdfd2ae&style=feature:transit.line%7Celement:labels.text.fill%7Ccolor:0x8f7d77&style=feature:transit.line%7Celement:labels.text.stroke%7Ccolor:0xebe3cd&style=feature:transit.station%7Celement:geometry%7Ccolor:0xdfd2ae&style=feature:water%7Celement:geometry.fill%7Ccolor:0xb9d3c2&style=feature:water%7Celement:labels.text.fill%7Ccolor:0x92998d&size=480x360
-
-// {
-//   latitude: this.state.location.coords.latitude,
-//   longitude: this.state.location.coords.longitude,
-//   latitudeDelta: 0.0922,
-//   longitudeDelta: 0.0421,
-// }
-
-// {...this.state.markers.map(marker => (
-//   <MapView.Marker
-//     coordinate={marker.latlng}
-//     title={marker.title}
-//     description={marker.description}
-//   />
-// ))}
-
-              // {/* if within user's geofence
-              // if (pet.latitude && pet.longitude) { */}
-
-              // <Text>latitude: state {this.state.location.coords.latitude}, region {this.state.region.latitude}</Text>
-              // <Text>longitude: state {this.state.location.coords.longitude}, region {this.state.region.longitude}</Text>
-              // <Text>latitudeDelta: {this.state.region.latitudeDelta},</Text>
-              // <Text>longitudeDelta: {this.state.region.longitudeDelta},</Text>
